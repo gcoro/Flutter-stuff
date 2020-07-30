79c4698295e5b77e85530467a1cd4ad87da8de7c
gcoro <giulia994@ymail.com>
2020-07-30 18:02:28 +0200

    [cat_app] fix url validation

--- cat_app/lib/screens/qr_scanning_page.dart
+++ cat_app/lib/screens/qr_scanning_page.dart
@@ -42,7 +42,10 @@ class _QrScanningPageState extends State<QrScanningPage> {
   }
 
   _isURL(String url) {
-    return Uri.parse(url).isAbsolute;
+    var urlPattern =
+        r"(https?|http)://([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?";
+    var match = RegExp(urlPattern, caseSensitive: false).hasMatch(url);
+    return match;
   }
 
   @override
